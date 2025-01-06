9568087905704c58b11dd88d1c364ffdbc297855
EduFayet <etienne.du-fayet@entalpic.ai>
2025-01-06 21:48:27 +0100

    added all etienne experiements, using the ABC for the hash/pymatgen, and fit in the Notebook

--- src/material_hasher/similarity/structure_matchers.py
+++ src/material_hasher/similarity/structure_matchers.py
@@ -3,7 +3,13 @@ from typing import List, Optional
 from pymatgen.analysis.structure_matcher import StructureMatcher
 from pymatgen.core.structure import Structure
 
-from material_hasher.similarity.base import SimilarityMatcherBase
+from material_hasher.similarity.base import SimilarityMatcherBase, HashingMatcherBase
+from material_hasher.hasher.entalpic import EntalpicMaterialsHasher
+
+
+def shorten_hash(hash):
+    split = hash.split("_")
+    return split[0] + "_" + split[2]
 
 
 class PymatgenStructureSimilarity(SimilarityMatcherBase):
@@ -59,3 +65,49 @@ class PymatgenStructureSimilarity(SimilarityMatcherBase):
             Similarity score between the two structures.
         """
         return self.matcher.get_rms_dist(structure1, structure2)
+
+
+class EntalpicStructureMatcher(HashingMatcherBase):
+    """Implementation of the StructureMatcherBase using the EntalpicmaterialHasher."""
+
+    def __init__(self, tolerance=0.01):
+        self.tolerance = tolerance
+        self.matcher = EntalpicMaterialsHasher()
+        self.structures: List[Structure] = []
+
+    def get_hash(self, structure: Structure) -> str:
+        return self.matcher.get_material_hash(structure)
+
+    def are_similar(
+        self,
+        structure1: Structure,
+        structure2: Structure,
+        threshold: Optional[float] = None,
+        get_shorten_hash: bool = False,
+    ) -> bool:
+        """
+        Check if two structures are similar based on the StructureMatcher of
+        pymatgen. The StructureMatcher uses a similarity algorithm based on the
+        maximum common subgraph isomorphism and the Jaccard index of the sites.
+
+        Parameters
+        ----------
+        structure1 : Structure
+            First structure to compare.
+        structure2 : Structure
+            Second structure to compare.
+
+        Returns
+        -------
+        bool
+            True if the two structures are similar, False otherwise.
+        """
+
+        hash_structure1 = self.get_hash(structure1)
+        hash_structure2 = self.get_hash(structure2)
+
+        if get_shorten_hash:
+            hash_structure1 = shorten_hash(hash_structure1)
+            hash_structure2 = shorten_hash(hash_structure2)
+
+        return hash_structure1 == hash_structure2

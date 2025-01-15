d87bd45a762f4cdb66dfba95bf3f1cf28413cd31
amandinerossello <amandine.rossello@entalpic.ai>
2025-01-15 19:25:09 +0100

    updated docstrings

--- src/material_hasher/benchmark/transformations.py
+++ src/material_hasher/benchmark/transformations.py
@@ -8,21 +8,21 @@ import numpy as np
 from pymatgen.core import Structure, SymmOp
 
 ALL_TEST_CASES = [
-    "gaussian_noise",
+    #"gaussian_noise",
     # "isometric_strain",
     # "strain",
     # "translation",
-    #"symm_ops",
+    "symm_ops",
 ]
 
 #    "gaussian_noise": {"sigma": np.arange(0, 0.31, 0.01).tolist()},
 
 PARAMETERS = {
-    "gaussian_noise": {"sigma": [0, 0.001, 0.003]},
+    #"gaussian_noise": {"sigma": [0, 0.001, 0.003]},
     # "isometric_strain": {"pct": [0.8, 1.0, 1.2]},
     # "strain": {"sigma": [0.01, 0.1, 0.3]},
     # "translation": {"sigma": [0.01, 0.1, 0.3]},
-    #"symm_ops": {"structure_symmetries" : ['all_symmetries_found']},  
+    "symm_ops": {"structure_symmetries" : ['all_symmetries_found']},  
 }
 
 
@@ -94,7 +94,7 @@ def get_new_structure_with_translation(structure: Structure, sigma: float) -> St
 def get_new_structure_with_symm_ops(
     structure: Structure,
     structure_symmetries: str,
-) -> Union[list[Structure]]:
+) -> list[Structure]:
     """
     Modify a structure using symmetry operations.
 
@@ -102,14 +102,13 @@ def get_new_structure_with_symm_ops(
     ----------
     structure : Structure
         Input structure to modify.
-    symm_ops : str, optional
-        Determines how symmetry operations are applied. Can be 'all' to apply
-        all symmetries, or 'random' to apply one symmetry operation. Defaults to 'random'.
+    structure_symmetries : str, optional
+        Determines how symmetry operations are applied. It apply all symmetries by default.
 
     Returns
     -------
-    Union[Structure, list[Structure]]
-        A single modified structure if 'random' is chosen, or a list of modified structures if 'all' is chosen.
+    list[Structure]
+        A list of modified structures.
 
     Raises
     ------

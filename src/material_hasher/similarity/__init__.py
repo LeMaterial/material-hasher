379c4b5193d8368ecda2010c1caaed2b24a37169
Ali Ramlaoui <ramlaoui@outlook.com>
2025-01-17 12:10:44 +0100

    Fix optional dependencies bugs

--- src/material_hasher/similarity/__init__.py
+++ src/material_hasher/similarity/__init__.py
@@ -1,10 +1,20 @@
 # Copyright 2025 Entalpic
-# from .eqv2 import EquiformerV2Similarity
+import logging
+
 from .structure_matchers import PymatgenStructureSimilarity
 
-# __all__ = ["EquiformerV2Similarity", "PymatgenStructureSimilarity"]
+__all__ = ["PymatgenStructureSimilarity"]
 
 SIMILARITY_MATCHERS = {
-    # "eqv2": EquiformerV2Similarity,
     "pymatgen": PymatgenStructureSimilarity,
 }
+
+try:
+    from .eqv2 import EquiformerV2Similarity
+
+    __all__.append("EquiformerV2Similarity")
+    SIMILARITY_MATCHERS["eqv2"] = EquiformerV2Similarity  # type: ignore
+except ImportError:
+    logging.warning(
+        "EquiformerV2Similarity is not available. This is a known issue on MacOS. Otherwise you need to installed the optional dependencies for this feature."
+    )

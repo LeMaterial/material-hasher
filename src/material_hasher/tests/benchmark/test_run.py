8cbe71537f41e924dfb0861355a73110a8305c0d
martinsiron <martin.siron@entalpic.ai>
2025-01-27 10:17:26 +0100

    fix test run to handle all test cases

--- src/material_hasher/tests/benchmark/test_run.py
+++ src/material_hasher/tests/benchmark/test_run.py
@@ -2,15 +2,17 @@ from material_hasher.benchmark.run_transformations import (
     diagram_sensitivity,
     get_data_from_hugging_face,
 )
+from material_hasher.benchmark.transformations import ALL_TEST_CASES
 
 
 # Example usage for TestBenchmark_HG
-class TestBenchmark_HG:
+class TestAllTransformationsBenchmark:
     def test_run(self):
-        hg_structures = get_data_from_hugging_face("token")
-        diagram_sensitivity(
-            hg_structures, "symm_ops", "TestDataset", "symmetries", "./output"
-        )
+        hg_structures = [get_data_from_hugging_face("token")[0]]
+        for test_case in ALL_TEST_CASES:
+            diagram_sensitivity(
+                hg_structures, test_case, "Test Dataset", test_case, "./output"
+            )
 
     def recup_mat(self):
         return get_data_from_hugging_face("token")
